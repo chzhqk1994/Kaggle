1859dd26d479aead36d48e6af97fb7f0d38fe7b5
hyunwoo <chzhqk1994@gmail.com>
2018-07-10 04:30:45 +0900

    titanic_survival_Cleaned Dataset

--- titanic_survival/titanic_survival.py
+++ titanic_survival/titanic_survival.py
@@ -1,7 +1,7 @@
 # Load in our libraries
 import pandas as pd
 import numpy as np
-import re
+import re  # re >> 정규표현 연산 re(Regular expression operation)
 import sklearn
 import xgboost as xgb  # xgboost : 그라디언트 부스트 라이브러리
 import seaborn as sns
@@ -80,11 +80,76 @@ for dataset in full_data:
     age_null_count = dataset['Age'].isnull().sum()
     age_null_random_list = np.random.randint(age_avg - age_std, age_avg + age_std, size=age_null_count)
 
-# 여기 무슨 말인지 모르겠다 진짜 모르겠다
+    # 여기 무슨 말인지 모르겠다 진짜 모르겠다
     dataset['Age'][np.isnan(dataset['Age'])] = age_null_random_list
     dataset['Age'] = dataset['Age'].astype(int)
 
-train['CategoricalFare'] = pd.cut(train['Age'], 5)
+train['CategoricalAge'] = pd.cut(train['Age'], 5)
 
-print(dataset)
+
+# 승객 이름으로 부터 타이틀 추출(정규표현식 좀 더 공부할 것)
+def get_title(name):
+    # ([A - Za - z] +)\.     >>>     정규표현식  영문만 가능하다는 의미
+    title_search = re.search(' ([A-Za-z]+)\.', name)
+
+    if title_search:
+        return title_search.group(1)  # list.group()  >> 정규표현식으로 매치된 문자열을 리턴
+
+    return ""
+
+
+# Title 이라는 새로운 피쳐 추가
+for dataset in full_data:
+    dataset['Title'] = dataset['Name'].apply(get_title)
+
+for dataset in full_data:
+    # replace(이거를, 이거로 바꾼다)
+    dataset['Title'] = dataset['Title'].replace(
+        ['Lady', 'Couness', 'Capt', 'Col', 'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Johnkheer', 'Dona'], 'Rare')
+
+    dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
+    dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
+
+for dataset in full_data:
+    # Sex 피쳐를 매핑(?) 하여 여성은 0, 남성은 1 로 지정
+    dataset['Sex'] = dataset['Sex'].map({'female': 0, 'male': 1}).astype(int)
+
+    title_mapping = {'Mr': 1, 'Miss': 2, 'Mrs': 3, 'Master': 4, 'Rare': 5}
+    # 위에서 지정한 매핑(?) 값으로 텍스트를 수치데이터로 치환
+    dataset['Title'] = dataset['Title'].map(title_mapping)
+    # Title 피처를 검사하며 NaN 값이 있으면 0으로 치환
+    dataset['Title'] = dataset['Title'].fillna(0)
+
+    # Title 값이 부동소수점 형식으로 나와서 임의로 추가
+    dataset['Title'] = dataset['Title'].astype(int)
+
+    dataset['Embarked'] = dataset['Embarked'].map({'S': 0, 'C': 1, 'Q': 2}).astype(int)
+
+    # Mapping fare
+    # loc 인덱서 >>  리스트.loc[행 인덱스, 열 인덱스]  >> 행, 열 인덱스에 따른 값을 찾아냄
+    # 여기서는   >>  리스트.loc[행 인덱스 조건, 열 인덱스]  >> 행, 열 인덱스에 따른 값을 찾아냄
+    # 왠지는 모르겠는데 여기서는 조건식에서 and 가 아니라 & 을 써야함!!!
+    dataset.loc[dataset['Fare'] <= 7.91, 'Fare'] = 0
+    dataset.loc[(7.91 < dataset['Fare']) & (dataset['Fare'] <= 14.454), 'Fare'] = 1
+    dataset.loc[(14.454 < dataset['Fare']) & (dataset['Fare'] <= 31), 'Fare'] = 2
+    dataset.loc[dataset['Fare'] > 31, 'Fare'] = 3
+    dataset['Fare'] = dataset['Fare'].astype(int)
+
+    dataset.loc[dataset['Age'] <= 16, 'Age'] = 0
+    dataset.loc[(16 < dataset['Age']) & (dataset['Age'] <= 32), 'Age'] = 1
+    dataset.loc[(32 < dataset['Age']) & (dataset['Age'] <= 48), 'Age'] = 2
+    dataset.loc[(48 < dataset['Age']) & (dataset['Age'] <= 64), 'Age'] = 3
+    dataset.loc[64 < dataset['Age'], 'Age'] = 4
+
+
+# Feature selection
+drop_element = ['PassengerId', 'Name', 'Ticket', 'Cabin', 'SibSp']
+train = train.drop(drop_element, axis=1)
+train = train.drop(['CategoricalAge', 'CategoricalFare'], axis=1)
+
+test = test.drop(drop_element, axis=1)
+
+print(train.head(3))
+
 
